1b16e89f0ef3a96e4528276aaac4fbf5c15e8ecc
Laís Dare <laisdarem@gmail.com>
2026-06-04 18:50:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,299 @@
+const terminalText = document.getElementById("terminalText");
+const startBtn = document.getElementById("startBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const bootScreen = document.getElementById("boot-screen");
+const content = document.getElementById("content");
+const nextContainer = document.getElementById("nextContainer");
+
+const typedCommand = document.getElementById("typed-command");
+
+let etapa = 0;
+
+const telas = [
+
+`$ cat thoughts.txt
+
+Se eu fosse construir uma casa.`,
+
+`Loading structure...
+
+Eu usaria os seus braços como paredes.`,
+
+`Loading windows...
+
+Seus olhos como janelas.`,
+
+`Loading entrance...
+
+Seu sorriso como porta da frente.`,
+
+`Loading fireplace...
+
+Seu coração como a lareira.`,
+
+`Loading light...
+
+E tua alma como a luz.`,
+
+`Saving faith...
+
+E por fim, eu depositaria minha fé.`,
+
+`Verifying...
+
+Sabendo que finalmente`,
+
+];
+
+function typeCommand() {
+
+    const command = "$ mkdir home";
+
+    let i = 0;
+
+    const interval = setInterval(() => {
+
+        const current = command.substring(0, i);
+
+        const commandPart = current.replace("$ ", "");
+
+        let homePart = "";
+
+        if (commandPart.includes(" ")) {
+            homePart = commandPart.split(" ").slice(1).join(" ");
+        }
+
+        const mkdirPart = commandPart.includes(" ")
+            ? commandPart.split(" ")[0]
+            : commandPart;
+
+        typedCommand.innerHTML = `
+            <span class="green">$</span>
+            <span class="red">${mkdirPart}</span>
+            <span class="white">${homePart ? " " + homePart : ""}</span>
+        `;
+
+        i++;
+
+        if (i > command.length) {
+            clearInterval(interval);
+        }
+
+    }, 120);
+
+}
+
+function digitar(texto, callback) {
+
+    terminalText.textContent = "";
+
+    let i = 0;
+
+    const intervalo = setInterval(() => {
+
+        terminalText.textContent += texto.charAt(i);
+
+        i++;
+
+        if (i >= texto.length) {
+
+            clearInterval(intervalo);
+
+            callback();
+
+        }
+
+    }, 30);
+
+}
+
+function mostrarEtapa() {
+
+    nextContainer.classList.add("hidden");
+
+    digitar(telas[etapa], () => {
+
+        nextBtn.textContent =
+            etapa === telas.length - 1
+                ? "Descobrir →"
+                : "Continuar →";
+
+        nextContainer.classList.remove("hidden");
+
+    });
+
+}
+
+startBtn.addEventListener("click", () => {
+
+    bootScreen.classList.add("hidden");
+
+    content.classList.remove("hidden");
+
+    mostrarEtapa();
+
+});
+
+nextBtn.addEventListener("click", () => {
+
+   if (etapa === telas.length - 1) {
+
+    terminalText.innerHTML = `
+<div class="final-screen">
+
+    <div class="final-text">
+        Encontrei um lar
+    </div>
+
+    <div id="slideshow">
+        <img id="currentPhoto" src="img/foto1.jpeg">
+    </div>
+
+    <div id="photo-gallery" class="photo-gallery hidden">
+
+        <div class="polaroid hidden-photo rotate-left">
+            <img src="img/foto1.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-right">
+            <img src="img/foto10.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-left">
+            <img src="img/foto3.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-right">
+            <img src="img/foto4.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-left">
+            <img src="img/foto5.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-right">
+            <img src="img/foto6.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-left">
+            <img src="img/foto7.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-right">
+            <img src="img/foto8.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-left">
+            <img src="img/foto9.jpeg">
+        </div>
+
+        <div class="polaroid hidden-photo rotate-right">
+            <img src="img/foto2.jpeg">
+        </div>
+
+    </div>
+
+    <div class="signature">
+        — Laís & Móises
+            05/02/2026
+                ❤️
+    </div>
+
+</div>
+`;
+
+    const music = document.getElementById("music");
+
+    music.volume = 0.5;
+
+    music.play().catch(() => {
+        console.log("Autoplay bloqueado pelo navegador.");
+    });
+
+    const imagens = [
+        "img/foto1.jpeg",
+        "img/foto10.jpeg",
+        "img/foto3.jpeg",
+        "img/foto4.jpeg",
+        "img/foto5.jpeg",
+        "img/foto6.jpeg",
+        "img/foto7.jpeg",
+        "img/foto8.jpeg",
+        "img/foto9.jpeg",
+        "img/foto2.jpeg"
+    ];
+
+    const fotoAtual = document.getElementById("currentPhoto");
+    const slideshow = document.getElementById("slideshow");
+    const galeria = document.getElementById("photo-gallery");
+
+    fotoAtual.style.maxHeight = "350px";
+    fotoAtual.style.maxWidth = "750px"; 
+
+    let indice = 0;
+
+    const intervalo = setInterval(() => {
+
+    fotoAtual.style.opacity = 0;
+
+    setTimeout(() => {
+
+        indice++;
+
+        if (indice < imagens.length) {
+
+            fotoAtual.src = imagens[indice];
+
+            if (
+                imagens[indice].includes("foto10.jpeg") ||
+                imagens[indice].includes("foto2.jpeg")
+            ){
+                fotoAtual.style.maxWidth = "700px";
+            } else {
+                fotoAtual.style.maxWidth = "750px";
+            }
+
+            fotoAtual.style.opacity = 1;
+
+        } else {
+
+            clearInterval(intervalo);
+
+            slideshow.remove();
+
+            galeria.classList.remove("hidden");
+
+            const fotos = document.querySelectorAll(".hidden-photo");
+
+            fotos.forEach((foto, index) => {
+
+                setTimeout(() => {
+
+                    foto.classList.add("show-photo");
+
+                }, index * 250);
+
+            });
+
+        }
+
+    }, 500);
+
+}, 2000);
+
+    nextContainer.classList.add("hidden");
+
+    return;
+}
+    etapa++;
+    mostrarEtapa();
+
+});
+
+window.onload = () => {
+
+    typeCommand();
+
+};
+
